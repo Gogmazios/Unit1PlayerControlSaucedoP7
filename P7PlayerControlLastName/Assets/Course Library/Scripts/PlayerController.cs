13f77094abd3264ef9d2a0fc346120c39003d973
Gogmazios <732861@student.dusd.net>
2026-01-30 15:27:38 -0800

    HUH
    did everything now i just need to add the priomids and other things, you know?

---
+++ P7PlayerControlLastName/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private float speed = 35.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput; 
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //moos ze tank on forward y forwards
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //moves the tank from side to side aka makes it possibly do the cha cha side
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+       
+    }
+}
